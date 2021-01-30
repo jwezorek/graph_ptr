2dbecd72db2cf532028f669b1521a0207e38f859
jwezorek <jwezorek@bluebeam.com>
2021-01-29 16:10:43 -0800

    some ideas stubbed in

--- src/graph_ptr.hpp
+++ src/graph_ptr.hpp
@@ -1,5 +1,54 @@
 #pragma once
 
-class A {
+#include <vector>
+#include <tuple>
+#include <memory>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+
+namespace gp {
+
+    namespace detail {
+
+        template<typename T>
+        class graph
+        {
+        public:
+
+        private:
+        };
+
+    }
+
+    template<typename... Ts> 
+    class graph_pool {
+
+    public:
+
+        template<typename T>
+        class graph_ptr {
+            friend class graph_pool;
+        private:
+
+            graph_ptr(graph_pool& gp, T* p) : pool_(gp), v_(p), u_(nullptr) {}
+
+            T* v_;
+            void* u_;
+            graph_pool& pool_;
+        };
+
+        template<typename T, typename... Us>
+        graph_ptr<T> make(Us... args) {
+            auto& p = std::get<std::vector<std::unique_ptr<T>>>(pools_);
+            p.emplace_back(std::make_unique<T>(std::forward<Us...>(args...)));
+            return graph_ptr(*this, p.back().get());
+        }
+
+    private:
+
+        std::unordered_set<void*> roots_;
+        std::tuple<std::vector<std::unique_ptr<Ts>>...> pools_;
+    };
 
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,40 @@
 #include "graph_ptr.hpp"
 #include <iostream>
+#include <string>
 
-int main() {
-    A a;
-    std::cout << "Hello World!";
+struct a {
+    a(std::string msg) : msg_(msg) {}
+    std::string msg_;
+
+    ~a() {
+        std::cout << "  destroying A{ " << msg_ << " }\n";
+    }
+};
+
+struct b {
+    b(std::string msg) : msg_(msg) {}
+    std::string msg_;
+
+    ~b() {
+        std::cout << "  destroying B{ " << msg_ << " }\n";
+    }
+};
+
+struct c {
+
+};
+
+int main() { 
+    std::cout << "making graph\n";
+
+    {
+        gp::graph_pool<a, b, c> p;
+
+        auto a_ptr = p.make<a>(std::string( "foo" ));
+        auto b_ptr = p.make<b>(std::string( "bar" ));
+    }
+
+    std::cout << "done\n";
+
     return 0;
 }
